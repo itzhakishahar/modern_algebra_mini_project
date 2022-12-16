c1361b907de183503f2148abdfa5f421affea7a5
LocalTed <ilyapoly321@gmail.com>
2022-12-16 12:31:25 +0200

    g

--- question_5.py
+++ question_5.py
@@ -6,11 +6,13 @@ q = 6841
 N = p*q
 phi = (q-1) * (p-1)
 a = number_theory_functions.randrange(0, phi)
-e = rsa_functions.RSA.generate_e(a, phi)
+e = rsa_functions.RSA.generate_e(50, phi)
 d = number_theory_functions.modular_inverse(e, phi)
 rsa = rsa_functions.RSA((N,e), (N,d))
 print("e:")
 print(e)
+print("inverse of e:")
+print(d)
 M = 42
 encrypted = rsa_functions.RSA.encrypt(rsa, 42)
 print("encrypted:")
